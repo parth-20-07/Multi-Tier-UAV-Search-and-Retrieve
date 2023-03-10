36f0a082811f22b3df4046d6a37ff2a3e4d45c3e
Parth Patel <parth.pmech@gmail.com>
2023-03-10 13:27:28 -0500

    Generate new Trajectory Generation Equation

--- Unity Project/Assets/Code/Scripts/MasterController.cs
+++ Unity Project/Assets/Code/Scripts/MasterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
@@ -30,51 +31,61 @@ public class MasterController : MonoBehaviour
 
     #region CustomMethod
 
-    UnityEngine.Vector3 Traj_evaluate(float t)
+    UnityEngine.Vector3[] Traj_evaluate(float t, Mathf mathf)
     {
-        int t0, tf, x0, y0, z0, xf, yf, zf, v0, vf, ac0, acf;
+        UnityEngine.Vector3 pos = UnityEngine.Vector3.zero;
+        UnityEngine.Vector3 vel = UnityEngine.Vector3.zero;
+        UnityEngine.Vector3 acc = UnityEngine.Vector3.zero;
 
+        float px, py, pz, vx, vy, vz, ax, ay, az;
         if (t <= 70)
         {
             if (t <= 5)
             {
-                UnityEngine.Vector3 pos = new(0, (0.08 * Mathf.Pow(t, 3)) - (0.024 * Mathf.Pow(t, 4)) + (0.0019 * Mathf.Pow(t, 5)), 0);
+                px = 0;
+                py = (float)((0.08 * Math.Pow(t, 3)) - (0.024 * Math.Pow(t, 4)) + (0.0019 * Math.Pow(t, 5)));
+                pz = 0;
+                vx = 0;
+                vy = (float)((0.024 * Math.Pow(t, 2)) - (0.096 * Math.Pow(t, 3)) + (0.0096 * Math.Pow(t, 4)));
+                vz = 0;
+                ax = 0;
+                ay = (float)((0.48 * t) - (0.288 * Math.Pow(t, 2)) + (0.0348 * Math.Pow(t, 3)));
+                az = 0;
 
+                pos = new UnityEngine.Vector3(px, py, pz);
+                vel = new UnityEngine.Vector3(vx, vy, vz);
+                acc = new UnityEngine.Vector3(ax, ay, az);
 
-                t0 = 0; tf = 5; x0 = 0; y0 = 0; z0 = 0; xf = 0; yf = 0; zf = 1; v0 = 0; vf = 0; ac0 = 0; acf = 0;
                 Debug.Log("Lift OFF - T:" + Mathf.Round(t));
             }
             else if (t <= 20)
             {
-                t0 = 5; tf = 20; x0 = 0; y0 = 0; z0 = 1; xf = 1; yf = 0; zf = 1; v0 = 0; vf = 0; ac0 = 0; acf = 0;
                 Debug.Log("Path 1 - T:" + Mathf.Round(t));
             }
             else if (t <= 35)
             {
-                t0 = 20; tf = 35; x0 = 1; y0 = 0; z0 = 1; xf = 1; yf = 1; zf = 1; v0 = 0; vf = 0; ac0 = 0; acf = 0;
                 Debug.Log("Path 2 - T:" + Mathf.Round(t));
             }
             else if (t <= 50)
             {
-                t0 = 35; tf = 50; x0 = 1; y0 = 1; z0 = 1; xf = 0; yf = 1; zf = 1; v0 = 0; vf = 0; ac0 = 0; acf = 0;
                 Debug.Log("Path 3 - T:" + Mathf.Round(t));
             }
             else if (t <= 65)
             {
-                t0 = 50; tf = 65; x0 = 0; y0 = 1; z0 = 1; xf = 0; yf = 0; zf = 1; v0 = 0; vf = 0; ac0 = 0; acf = 0;
                 Debug.Log("Path 4 - T:" + Mathf.Round(t));
             }
             else
             {
-                t0 = 65; tf = 70; x0 = 0; y0 = 0; z0 = 1; xf = 0; yf = 0; zf = 0; v0 = 0; vf = 0; ac0 = 0; acf = 0;
                 Debug.Log("Path 5 - T:" + Mathf.Round(t));
             }
         }
         else
         {
-            t0 = 65; tf = 70; x0 = 0; y0 = 0; z0 = 1; xf = 0; yf = 0; zf = 0; v0 = 0; vf = 0; ac0 = 0; acf = 0;
-            Debug.Log("Path 5 - T:" + Mathf.Round(t));
+            Debug.Log("Stable - T:" + Mathf.Round(t));
         }
-        #endregion//CustomMethod
+
+        UnityEngine.Vector3[] return_val = { pos, vel, acc };
+        return return_val;
     }
+    #endregion//CustomMethod
 }
